5f8898708b30c8877db2170f168147e08cf49111
Nicolas <nicolas.jreichert@gmail.com>
2016-08-25 12:43:54 +0200

    Initial Commit

---
+++ Duplicate/Duplicate.h
@@ -0,0 +1,13 @@
+//
+//  Duplicate.h
+//  Duplicate
+//
+//  Created by Nicolas Reichert on 25/08/16.
+//  Copyright © 2016 Nicolas Reichert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Duplicate : NSObject
+
+@end
---
+++ Duplicate/Duplicate.m
@@ -0,0 +1,13 @@
+//
+//  Duplicate.m
+//  Duplicate
+//
+//  Created by Nicolas Reichert on 25/08/16.
+//  Copyright © 2016 Nicolas Reichert. All rights reserved.
+//
+
+#import "Duplicate.h"
+
+@implementation Duplicate
+
+@end
